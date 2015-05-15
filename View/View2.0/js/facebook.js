45063743e6d89f9dd2d95389b0bb39f93ff6081e
beatrizsvs <beatrizsvs@hotmail.com>
2015-05-15 20:54:53 -0300

    View2.0

---
+++ View/View2.0/js/facebook.js
@@ -0,0 +1,67 @@
+		// This is called with the results from from FB.getLoginStatus().
+  		function statusChangeCallback(response) {
+   			 console.log('statusChangeCallback');
+    		 console.log(response);
+		    // The response object is returned with a status field that lets the
+		    // app know the current login status of the person.
+		    // Full docs on the response object can be found in the documentation
+		    // for FB.getLoginStatus().
+		    if (response.status === 'connected') {
+		    	alert("Conectou porra");
+		    } 
+		    else if (response.status === 'not_authorized') {
+		      // The person is logged into Facebook, but not your app.
+		      document.getElementById('status').innerHTML = 'Please log ' +
+		        'into this app.';
+
+		    } 
+		    else {
+		      // The person is not logged into Facebook, so we're not sure if
+		      // they are logged into this app or not.
+		      document.getElementById('status').innerHTML = 'Please log ' +
+		        'into Facebook.';
+		    }
+  		}
+
+  		// This function is called when someone finishes with the Login
+ 		// Button.  See the onlogin handler attached to it in the sample
+  		// code below.
+  		function checkLoginState() {
+   			
+   			FB.getLoginStatus(function(response) {
+      		 	statusChangeCallback(response);
+    		});
+
+ 	    }
+
+ 	    window.fbAsyncInit = function() {
+ 	    
+  			FB.init({
+
+	   			appId      : '1440324569613365',
+	    		cookie     : true,  // enable cookies to allow the server to access 
+	                        // the session
+	   		    xfbml      : true,  // parse social plugins on this page
+	    	    version    : 'v2.3' // use version 2.2
+  			}); 			
+
+		};
+
+
+
+		// Load the SDK asynchronously
+  		(function(d, s, id) {
+   			 var js, fjs = d.getElementsByTagName(s)[0];
+    		 if (d.getElementById(id)) return;
+    		 js = d.createElement(s); js.id = id;
+    		 js.src = "//connect.facebook.net/pt_BR/sdk.js";
+    		 fjs.parentNode.insertBefore(js, fjs);
+  		}(document, 'script', 'facebook-jssdk'));
+
+		(function(d, s, id) {
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) return;
+			 js = d.createElement(s); js.id = id;
+     		 js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3&appId=1440324569613365";
+ 			 fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
---
+++ View/View2.0/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>C'mon</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  <script language="javascript" src="js/facebook.js"></script>
+
+  </head>
+    <body>
+      <div class="container-fluid">
+        <header class="row">
+     
+        </header>
+
+        <div class="row">
+          <div role="main" class="col-md-7">
+            
+          </div>
+          <aside role="complementary" class="col-md-3">
+            
+          </aside>
+        </div>
+        
+        <footer class="row">
+       
+        </footer>
+        </div>
+
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+
+    </body>
+</html>
