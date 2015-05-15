d37420a04f1c07e6c6af8bf6e81ea8d9736fd636
Carlos <carlosadsds@gmail.com>
2015-05-14 21:47:28 -0300

    Atualizações nas Classes do Banco de Dados
    Correções de sintaxe e de lógica.
    Comentários no código-fonte.
    Substituição de "e-mail" por "id", nas classes em que o primeiro era
    utilizado.

--- Model/eventoDAO.php
+++ Model/eventoDAO.php
@@ -1,26 +1,42 @@
 <?php 
-		include_once 'conexao.php';
-	?>
+	//Inclusão da Classe de Conexão com o Banco de Dados.
+	include_once 'conexao.php';
+?>
 	
-	
-	<?php 
-	
-			
-	//cadastrar evento no banco
-	function inserirEvento ($nome,$local,$data,$horainicio,$horatermino,$minimoparticipantes,$autocancelamento,$urlimagem,$emailcriador,$observacoes,$lembrete){
-	$cadastro = mysql_query ("INSERT INTO evento (`nome`, `local`, `data`, `horainicio`, `horatermino`, `minimoparticipantes`, `autocancelamento`, `urlimagem`, `emailcriador`, `observacoes`, `lembrete`) VALUES ('$nome', '$local', '$data', '$horainicio', '$horatermino', '$minimoparticipantes', '$autocancelamento', '$urlimagem', '$emailcriador', '$observacoes', '$lembrete' )");
+<?php 		
+	public class EventoDAO{ //Classe de Acesso a Dados referentes ao Evento
+
+		//Cadastrar evento no Banco de Dados.
+		public function inserirEvento ($nome, $local, $data, $horainicio, $horatermino, $minimoparticipantes, $autocancelamento, $urlimagem, $idCriador, $observacoes, $lembrete){
+			$cadastro = mysql_query ("INSERT INTO evento (`nome`, `local`, `data`, `horainicio`, `horatermino`, `minimoparticipantes`, `autocancelamento`, `urlimagem`, `idCriador`, `observacoes`, `lembrete`) VALUES ('$nome', '$local', '$data', '$horainicio', '$horatermino', '$minimoparticipantes', '$autocancelamento', '$urlimagem', '$idCriador', '$observacoes', '$lembrete')"); //Insere um evento na tabela.
+			if(!$cadastro){ //Caso o cadastro do evento não seja efetuado por algum motivo, a função retorna "false".
+				return false;
+			}
+			return true; //Caso o cadastro do evento seja efetuado com sucesso, a função retorna "true".
+		}
+		
+		//Buscar evento no Banco de Dados.
+		public function buscarEventos ($id){
+			$buscar = mysql_query ("SELECT * FROM evento WHERE id=$id"); //Busca os eventos na tabela.
+			return $buscar; //Retorna os eventos da tabela.
+		}
+
+		//Editar evento no Banco de Dados.
+		public function editarEvento ($id, $nome, $local, $data, $horainicio, $horatermino, $minimoparticipantes, $autocancelamento, $urlimagem, $observacoes){
+			$editar_evento = mysql_query ("UPDATE evento SET nome=$nome, local=$local, data=$data,horainicio=$horainicio, horatermino=$horatermino, minimoparticipantes=$minimoparticipantes, autocancelamento=$autocancelamento,urlimagem=$urlimagem, observacoes=$observacoes WHERE id=$id"); //Modifica as informações do evento.
+			if(!$editar_evento){ //Caso a modificação não seja realizada por algum motivo, a função retorna "false".
+				return false;
+			}
+			return true; //Caso a alteração no evento seja efetuada com sucesso, a função retorna "true".
+		}
+		
+		//Deletar evento do Banco de Dados.
+		public function deletarEvento ($id){
+			$deletar = mysql_query ("DELETE FROM evento WHERE id=$id"); //Deleta um evento da tabela.
+			if(!$deletar){ //Caso o evento não seja apagado por algum motivo, a função retorna "false".
+				return false;
+			}
+			return true; //Caso a exclusão do evento seja efetuada com sucesso, a função retorna "true".
+		}
 	}
-	//buscar evento 
-	function buscarEvento ($id){
-	$buscar = mysql_query ("SELECT * FROM evento WHERE id=$id");
-	return $buscar;
-	}
-	//editar evento
-	function editarEvento ($id){
-	$editar_evento = mysql_query ("UPDATE evento SET nome=$nome, local=$local, data=$data,horainicio=$horainicio, horatermino=$horatermino, minimoparticipantes=$minimoparticipantes, autocancelamento=$autocancelamento,urlimagem=$urlimagem, observacoes=$observacoes, lembrete=$lembrete WHERE id=$id ");
-	}
-	function deletarEvento($id){
-	//deletar evento
-	$deletar = mysql_query ("DELETE FROM evento WHERE id=$id");
-	}
-	?>
+?>
--- Model/usuarioDAO.php
+++ Model/usuarioDAO.php
@@ -1,26 +1,42 @@
 <?php 
-		include_once 'conexao.php';
-	?>
+	//Inclusão da Classe de Conexão com o Banco de Dados.
+	include_once 'conexao.php';
+?>
 	
-	
-	<?php 
-	
-			
-	function cadastrarUsuario ($email,$nome,$localizacao,$urlfoto,$idade,$esportefavorito){
-	//cadastrar usuario no banco
-	$cadastro = mysql_query ("INSERT INTO usuario ( `email`, `nome`, `localizacao`, `urlfoto`, `idade`, `esportefavorito`) VALUES ('$email', '$nome', '$localizacao', $urlfoto, $idade, $esportefavorito)");
+<?php 	
+	public class UsuarioDAO{ //Classe de Acesso a Dados referentes ao Usuário.
+		
+		//Cadastrar usuário no Banco de Dados.
+		public function cadastrarUsuario ($id, $nome, $cidade, $urlfoto, $idade, $esportefavorito){
+			$cadastro = mysql_query ("INSERT INTO usuario (`id`, `nome`, `cidade`, `urlfoto`, `idade`, `esportefavorito`) VALUES ('$id', '$nome', '$cidade', '$urlfoto', '$idade', '$esportefavorito')"); //Insere na tabela as informações que compõem o perfil do usuário.
+			if(!$cadastro){ //Caso o cadastro do usuário não seja efetuado por algum motivo, a função retorna "false".
+				return false;
+			}
+			return true; //Caso o cadastro do usuário seja efetuado com sucesso, a função retorna "true".
+		}
+
+		//Buscar usuário no Banco de Dados.
+		public function buscarUsuario ($id){
+			$buscar = mysql_query ("SELECT * FROM usuario WHERE id=$id"); //Busca os usuários na tabela.
+			return $buscar; //Retorna os usuários da tabela.
+		}
+
+		//Editar as informações do usuário no Banco de Dados.
+		public function editarUsuario ($id, $nome, $cidade, $urlfoto, $idade, $esportefavorito){
+			$editar_usuario = mysql_query ("UPDATE usuario SET nome=$nome, cidade=$cidade, urlfoto=$urlfoto, idade=$idade, esportefavorito=$esportefavorito WHERE id=$id"); //Atualiza na tabela as informações que compõem o perfil do usuário.
+			if(!$editar_usuario){ //Caso a modificação não seja realizada por algum motivo, a função retorna "false".
+				return false;
+			}
+			return true; //Caso a alteração nas informações do usuário seja efetuada com sucesso, a função retorna "true".
+		}
+
+		//Deletar usuário do Banco de Dados.
+		public function deletarUsuario ($id){
+			$deletar = mysql_query ("DELETE FROM usuario WHERE id=$id"); //Deleta um usuário da tabela.
+			if(!$deletar){ //Caso o usuário não seja apagado por algum motivo, a função retorna "false".
+				return false;
+			}
+			return true; //Caso a exclusão do usuário seja efetuada com sucesso, a função retorna "true".
+		}
 	}
-	function buscarUsuario ($id){
-	//buscar usuario
-	$buscar = mysql_query ("SELECT * FROM usuario WHERE id=$id");
-	return $buscar;
-	}
-	function editarEvento ($id,$esportefavorito){
-	//editar usuario
-	$editar_evento = mysql_query ("UPDATE usuario SET esportefavorito=$esportefavorito WHERE id=$id");
-	}
-	function deletarUsuario($id){
-	//deletar usuario
-	$deletar = mysql_query ("DELETE FROM usuario WHERE id=$id");
-	}
-	?>
+?>
