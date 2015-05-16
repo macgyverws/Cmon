40539a63eb9a96eda1855ff85f6d7df5e5a20d4c
IanVigano <ians028@gmail.com>
2015-05-15 21:46:56 -0300

    Update agenda.php

--- View/agenda.php
+++ View/agenda.php
@@ -12,6 +12,79 @@
   </head>
 
   <body>
+      <?php
+
+          ########## Google Settings.. Client ID, Client Secret from https://cloud.google.com/console #############
+          $google_client_id     = '1024869722973-7vf41jtl0nopiat2uktfjvt8tmr3f8cl.apps.googleusercontent.com';
+          $google_client_secret   = '_9W2EGbTPSgev-jK4N1EKLQh';
+          $google_redirect_url  = 'http://localhost/Cmon-master/View/agenda.php'; //path to your script
+          $google_developer_key   = 'AIzaSyAj6hOfVHeZCcH7Q6hC-1b-2TdsNGcUPiw';
+
+          ###################################################################
+
+          //include google api files
+          require_once 'src/Google_Client.php';
+          require_once 'src/contrib/Google_Oauth2Service.php';
+
+          //start session
+          session_start();
+
+          $gClient = new Google_Client();
+          $gClient->setApplicationName('Login to Sanwebe.com');
+          $gClient->setClientId($google_client_id);
+          $gClient->setClientSecret($google_client_secret);
+          $gClient->setRedirectUri($google_redirect_url);
+          $gClient->setDeveloperKey($google_developer_key);
+
+          $google_oauthV2 = new Google_Oauth2Service($gClient);
+
+          //If user wish to log out, we just unset Session variable
+          if (isset($_REQUEST['reset'])) 
+          {
+            unset($_SESSION['token']);
+            $gClient->revokeToken();
+            header('Location: ' . filter_var($google_redirect_url, FILTER_SANITIZE_URL)); //redirect user back to page
+          }
+
+          //If code is empty, redirect user to google authentication page for code.
+          //Code is required to aquire Access Token from google
+          //Once we have access token, assign token to session variable
+          //and we can redirect user back to page and login.
+          if (isset($_GET['code'])) 
+          { 
+            $gClient->authenticate($_GET['code']);
+            $_SESSION['token'] = $gClient->getAccessToken();
+            header('Location: ' . filter_var($google_redirect_url, FILTER_SANITIZE_URL));
+            return;
+          }
+
+
+          if (isset($_SESSION['token'])) 
+          { 
+            $gClient->setAccessToken($_SESSION['token']);
+          }
+
+
+          if ($gClient->getAccessToken()) 
+          {
+              //For logged in user, get details from google using access token
+              $user         = $google_oauthV2->userinfo->get();
+              $user_id        = $user['id'];
+              $user_name      = filter_var($user['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+              $email        = filter_var($user['email'], FILTER_SANITIZE_EMAIL);
+              $profile_url      = filter_var($user['link'], FILTER_VALIDATE_URL);
+              $profile_image_url  = filter_var($user['picture'], FILTER_VALIDATE_URL);
+              $personMarkup     = "$email<div><img src='$profile_image_url?sz=50'></div>";
+              $_SESSION['token']  = $gClient->getAccessToken();
+          }
+          else 
+          {
+            //For Guest user, get google login url
+            $authUrl = $gClient->createAuthUrl();
+          }
+      ?>
+
+
 
       <?php
           
@@ -49,12 +122,12 @@
 
       <div class="container">
         <header class="row">
-          <div class="navbar navbar-fixed-top navbar-inverse">
+          <!--<div class="navbar navbar-fixed-top navbar-inverse">
             <div class="navbar-inner">
               <a href="https://www.facebook.com/" target="parent" style="margin-left: 5px"> <img alt = "Página no Facebook" width = "45" height = "45" src="img/btnFacebook.png"> </a>
               <a href="https://www.twitter.com/" target="parent" style="margin-left: 5px;"> <img alt = "Página no Twitter" width = "45" height = "45" src="img/btnTwitter.jpg"> </a>
             </div>
-          </div>
+          </div>-->
         </header>
 
         <div class="row">
@@ -78,13 +151,24 @@
         
         <div class="row">
           <hr class="featurette-divider">
-          <div role="main2" class="col-md-6 column">
+          <div role="main2" class="col-md-4 column">
             <button type="button" class="btn btn-primary btn-lg" aria-label="right Align">
               <span class="glyphicon glyphicon-plus" aria-hidden="true">  Criar evento</span>
             </button>
           </div>
  
-          <div role="complementary2" class="col-md-6 column">
+          <div role="complementary2" class="col-md-4 column">
+             <?php if(isset($authUrl)) //user is not logged in, show login button
+              {
+                echo '<a class="login" href="'.$authUrl.'"><img src="images/google-login-button.png" /></a>';
+              } 
+              else // user logged in 
+              {
+                echo '<br /><a class="logout" href="?reset=1">Logout</a>';
+              }?>
+          </div>
+
+          <div role="complementary2" class="col-md-4 column">
             <button type="button" class="btn btn-primary btn-lg" aria-label="right Align">
               <span class="glyphicon glyphicon-calendar" aria-hidden="true">  Agenda</span>
             </button>
