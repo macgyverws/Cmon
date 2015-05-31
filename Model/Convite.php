3bee52b0aedd1c11f7bfaf3bbfc75a84c02de07b
franklinOliveira <franklinoliveira40@gmail.com>
2015-05-31 15:58:46 -0300

    Update Convite.php

--- Model/Convite.php
+++ Model/Convite.php
@@ -1,53 +1,50 @@
 <?php
-    require_once "Evento.php";
-    require_once "Usuario.php";
+
      /**
      * A classe convite apresenta os atributos e métodos responsáveis pela manipulação dos convites para os eventos.
      */
 
-    public class Convite{ 
-     private $id;
-     private $IDconvidado;
-     private $IDevento;
-     private $confirmado =  false;
+    class Convite{ 
+        private $id;
+        private $IDconvidado;
+        private $IDevento;
+        private $confirmado = false;
   
-      /**
-      * Construtor da classe convite.
-      */
-      public function __construct($IDconvidado, $IDevento){
-        $this->IDconvidado = $IDconvidado; 
-        $this->IDevento = $IDevento; //tava sem o cifrão
-     }
-     /**
-     *Método responsável por confirmar a participação de um usuário no evento.
-     */
-      public function confirmarParticipacao(){  
-        $confirmado = true;
-     }
-     
-     /**
-     *Método responsável por cancelar a participação de um usuário no evento.
-     */
-     public function cancelarParticipacao(){
-        $confirmado = false;
-     }
-  
-    public function getID(){
-      return $this->$id;
-    }
+         /**
+         *Método responsável por confirmar a participação de um usuário no evento.
+        */
+        public function setConfirmacao($confirmado){  
+          $this->confirmado = $confirmado;
+        }
+        
+        public function setID($id){
+          $this->id = $id;
+        }
 
-    public function getConfirmado(){
-      return $this->$confirmado;
-    }
-  
-    public function getIDEvento(){
-        return $this->$IDevento;
-    }
-    
-    public function getIDConvidado(){
-        return $this->$IDconvidado;
-    }
-  
+        public function setIDconvidado($IDconvidado){
+          $this->IDconvidado = $IDconvidado;
+        }
+
+        public function setIDevento($IDevento){
+          $this->IDevento = $IDevento;
+        }
+
+        public function getID(){
+          return $this->id;
+        }
+
+        public function getConfirmado(){
+          return $this->confirmado;
+        }
+      
+        public function getIDEvento(){
+            return $this->IDevento;
+        }
+        
+        public function getIDConvidado(){
+            return $this->IDconvidado;
+        }
+      
     }
 ?>
 
