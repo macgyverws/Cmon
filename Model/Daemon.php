089aeb31d58bc54fae56b8bce0365e7f973d6c08
kayo220 <kayo.santana@hotmail.com>
2015-06-08 18:15:24 -0300

    Update Daemon.php
    Alterações e comentários

--- Model/Daemon.php
+++ Model/Daemon.php
@@ -7,15 +7,16 @@ while(TRUE){
         $eventoDAO = new EventoDAO();
         $dataHora = date("d/m/Y h:i:s");
         echo $dataHora;
-        //Percorre o array de convites confirmados.
-        for($i = 0; $i < sizeof($this->convitesConfirmados); $i++){
-          //Acessa a linha onde o evento correspondente ao convite está armazenado.
-          $result = $eventoDAO->buscarEvento($this->convitesConfirmados[$i]->getIDEvento());
-          $linha = mysql_fetch_array($result);
+        //Percorre o array de eventos.
+        //criar array de eventos
+        for($i = 0; $i < sizeof($this->eventos); $i++){
+          //Acessa a linha onde o evento está armazenado.
+         // Acessar todos os eventos e pegar todos as datas e horarios iniciais
+         //
+         
           //Verifica se os eventos acontecerão na mesma data
-          if($linha['data'] == $data){
-            //Verifica a probabilidade de concidirem os horários
-            if( ($horaInicio <= $linha['dataHora']){
+          if($linha['data'] == $dataHora){//poderiamos mandar apenas uma vez no dia, ou apenas no dia anterior ao evento, n sendo necessário executar toda hora
+           if( ($dataHora <= $linha['data']){
                 
                 //enviar lembretes 
                 //mudar o campo dos eventos para dizer que já foi lembrado
